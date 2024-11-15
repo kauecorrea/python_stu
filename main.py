36d827d1b4864379c2c616e5f7c984415fb6f3b8
Kauê Corrêa <kauehenrique08@gmail.com>
2024-11-15 16:25:14 -0300

    input

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ peso = 71.2
 print(nome)
 print(idade)
 print(peso)
+
+#input
+
+nome = input("Digite seu nome:")
+idade = input("Digite sua idade:")
+peso = input("Digite o seu peso:")
+
+print(nome)
+print(idade)
+print(peso)
